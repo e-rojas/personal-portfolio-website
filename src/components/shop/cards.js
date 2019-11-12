ca24184f772c51980b68551e0ffeaeff904eee26
javascript01fan <ejrojas@icloud.com>
2019-11-11 19:20:21 -0500

    change

--- src/components/shop/cards.js
+++ src/components/shop/cards.js
@@ -1,28 +1,28 @@
 import React, { useState } from "react"
 
 export default function Cards() {
-    const [price, setPrice] = useState(0)
+  const [price, setPrice] = useState(0)
 
-    function sumTotal(addedPrice) {
-      setPrice(price + addedPrice)
-    }
+  function sumTotal(addedPrice) {
+    setPrice(price + addedPrice)
+  }
 
   const cardsInfo = [
     {
       name: "Bottle Cage",
-      price: 22.00,
+      price: 22.0,
       img:
         "https://livedemo00.template-help.com/wt_prod-22072/images/product-1-109x175.png",
     },
     {
       name: "Mount Carrier",
-      price: 15.00,
+      price: 15.0,
       img:
         "https://livedemo00.template-help.com/wt_prod-22072/images/product-2-168x175.png",
     },
     {
       name: "Commuter Carrier",
-      price: 32.00,
+      price: 32.0,
       img:
         "https://livedemo00.template-help.com/wt_prod-22072/images/product-3-208x175.png",
     },
@@ -40,52 +40,74 @@ export default function Cards() {
     },
     {
       name: "Bike Helmet",
-      price: 31.00,
+      price: 31.0,
       img:
         "https://livedemo00.template-help.com/wt_prod-22072/images/product-6-179x175.png",
     },
   ]
 
   return (
-      <div className='text-center'>
-          <h4 className={price !== 0  ? 'text-danger':'text-dark'} >Cart Total: ${price.toFixed(2)} </h4>
-          <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        maxWidth: "1600px",
-        margin: "auto",
-      }}
+    <div className="text-center">
+      <h4 className={price !== 0 ? "text-danger" : "text-dark"}>
+        Cart Total: ${price.toFixed(2)}{" "}
+      </h4>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          maxWidth: "1600px",
+          margin: "auto",
+        }}
       >
-      {cardsInfo.map((element, index) => {
-        return <Card sumTotal={sumTotal} name={element.name} key={index} price={element.price} image={element.img} />
-      })}
-    </div>
+        {cardsInfo.map((element, index) => {
+          return (
+            <Card
+              sumTotal={sumTotal}
+              name={element.name}
+              key={index}
+              price={element.price}
+              image={element.img}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
 
 const Card = props => {
-    let [active, setActive] = useState(true);
-    const handleClickCard = () => {
-        setActive(!active);
-        props.sumTotal(active ? props.price : -props.price)
-    };
+  let [active, setActive] = useState(true)
+  const handleClickCard = () => {
+    setActive(!active)
+    props.sumTotal(active ? props.price : -props.price)
+  }
 
   return (
-      <div
-      className={active ? " card alert alert-info" : " card alert alert-secondary"}
-        onClick={handleClickCard}
-          style={{ flexGrow: "3", flexBasis: "23rem",  color: 'black', cursor: 'pointer' }} >
+    <div
+      className={
+        active ? " card alert alert-info" : " card alert alert-secondary"
+      }
+      onClick={handleClickCard}
+      style={{
+        flexGrow: "3",
+        flexBasis: "23rem",
+        color: "black",
+        cursor: "pointer",
+      }}
+    >
       <div className="row">
         <div className="col-4">
-                <img style={{width:'100px',height:'100px'}} className="img-thumbnail" alt='product' src={props.image} />
-         
+          <img
+            style={{ width: "100px", height: "100px" }}
+            className="img-thumbnail"
+            alt="product"
+            src={props.image}
+          />
         </div>
-              <div className="col-8">
-                  <h5>{props.name}</h5>
-                  <span className='font-weight-bolder'>${props.price.toFixed(2)}</span>
+        <div className="col-8">
+          <h5>{props.name}</h5>
+          <span className="font-weight-bolder">${props.price.toFixed(2)}</span>
         </div>
       </div>
       <div className="row">
