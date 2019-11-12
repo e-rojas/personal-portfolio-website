7198e3f3a726d8355d54ef09437dfa4b7c7c94d0
javascript01fan <ejrojas@icloud.com>
2019-11-12 18:06:17 -0500

    carousel data added

--- src/components/shop/_carousel.js
+++ src/components/shop/_carousel.js
@@ -1,51 +1,57 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import Button from 'react-bootstrap/Button';
+import React from "react"
+import Carousel from "react-bootstrap/Carousel"
+import Button from "react-bootstrap/Button"
 const _carousel = () => {
-    return (
-            
-<Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://livedemo00.template-help.com/wt_prod-22072/images/slide-1.jpg"
-      alt="First slide"
-    />
-    <Carousel.Caption className="text-left mb-5" >
-      <h3>Cycler-Parts</h3>
-      <p>Wide variety of parts.</p>
-      <Button className='text-dark' variant="warning">SHOP NOW</Button>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://livedemo00.template-help.com/wt_prod-22072/images/slide-3.jpg"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption   className="text-left mb-5" >
-      <h3>Best Prices!!</h3>
-      <p>30% on first purchase.</p>
-      <Button className='text-dark' variant="warning">SHOP NOW</Button>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://livedemo00.template-help.com/wt_prod-22072/images/slide-2.jpg"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption  className="text-left mb-5" >
-      <h3>Free Shipping</h3>
-      <p>Fast parts delivery.</p>
-      <Button className='text-dark' variant="warning">SHOP NOW</Button>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-      
-    );
+  const info = [
+    {
+      alt: "first slide",
+      source:
+        "https://livedemo00.template-help.com/wt_prod-22072/images/slide-1.jpg",
+      title: "Cycler-Parts",
+      subTitle: "Wide variety of parts",
+      buttonMsg: "SHOP NOW",
+    },
+    {
+      alt: "first slide",
+      source:
+        "https://livedemo00.template-help.com/wt_prod-22072/images/slide-2.jpg",
+      title: "Best Prices",
+      subTitle: "30% Discount.",
+      buttonMsg: "SHOP NOW",
+    },
+    {
+      alt: "first slide",
+      source:
+        "https://livedemo00.template-help.com/wt_prod-22072/images/slide-3.jpg",
+      title: "Free Shipping",
+      subTitle: "30 days warranty.",
+      buttonMsg: "SHOP NOW",
+    },
+  ]
+  return (
+    <Carousel style={{marginTop:'70px'}}>
+      {info.map((element, index) => {
+        return (
+          <Carousel.Item key={index}>
+            <img
+              className="d-block w-100"
+              src={element.source}
+              alt={element.alt}
+            />
+            <Carousel.Caption className="text-left mb-5">
+              <h3> {element.title} </h3>
+              <p> {element.subTitle} </p>
+              <Button className="text-dark" variant="warning">
+                {" "}
+                {element.buttonMsg}
+              </Button>
+            </Carousel.Caption>
+          </Carousel.Item>
+        )
+      })}
+      })
+    </Carousel>
+  )
 }
 
-export default _carousel;
+export default _carousel
