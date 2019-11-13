a2ce53fc6d5706047ebe22328f00fbcbb4101fa4
javascript01fan <ejrojas@icloud.com>
2019-11-13 13:07:19 -0500

    update nav

--- src/components/header.js
+++ src/components/header.js
@@ -17,12 +17,12 @@ export default () => {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link>
-              <Link activeClassName="activado" className="nav-link " to="/">
+              <Link className="nav-link " to="/">
                 Home
               </Link>
             </Nav.Link>
             <Nav.Link>
-              <Link activeClassName="activado" className="nav-link " to="/blog">
+              <Link  className="nav-link " to="/blog">
                 Blog
               </Link>
             </Nav.Link>
