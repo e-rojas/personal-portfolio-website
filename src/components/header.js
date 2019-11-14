df5b60b81ad7362f33eb2b9edd58b16874cee7c2
javascript01fan <ejrojas@icloud.com>
2019-11-14 17:12:31 -0500

    udate on the movie page initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -7,44 +7,25 @@ import { NavDropdown, Navbar, Nav } from "react-bootstrap"
 export default () => {
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">
-          <Link className="navbar-brand" to="/">
-            Edgar Rojas
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link>
-              <Link className="nav-link " to="/">
-                Home
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link  className="nav-link " to="/blog">
-                Blog
-              </Link>
-            </Nav.Link>
-          </Nav>
-          <Nav>
-            <NavDropdown style={{marginRight:'100px'}} title="Portfolio" id="collasible-nav-dropdown">
-              <NavDropdown.Item>
-                <Link  className="nav-link text-dark " to="/todoApp">
-                  Todo App
-                </Link>
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item>
-                <Link className="nav-link text-dark " to="/shop">
-                  Bike Shop
-                </Link>
-              </NavDropdown.Item>
-            </NavDropdown>
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Link className="navbar-brand" to="/"> Edgar Rojas </Link>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Link activeClassName="activado" className="nav-link " to="/"> Home</Link>
+          <Link activeClassName="activado" className="nav-link " to="/blog"> Blog</Link>
+          <NavDropdown title="React Projects" id="collasible-nav-dropdown">
+            <Link className="nav-link text-dark " to="/todoApp"> Todo App</Link>
+            <NavDropdown.Divider />
+            <Link className="nav-link text-dark " to="/shop"> Bike Shop</Link>
+            <NavDropdown.Divider />
+            <Link className="nav-link text-dark " to="/movies"> Movie Info</Link>
+          </NavDropdown>
+          
+        </Nav>
+    
+      </Navbar.Collapse>
+    </Navbar>
     </div>
   )
 }
