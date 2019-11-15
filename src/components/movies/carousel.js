e01f3820ae98d886ebb359a1da5d0fb58474ca5a
javascript01fan <ejrojas@icloud.com>
2019-11-14 19:24:43 -0500

    tst

--- src/components/movies/carousel.js
+++ src/components/movies/carousel.js
@@ -3,43 +3,57 @@ import InfiniteCarousel from "react-leaf-carousel"
 export default function Carousel(props) {
   return (
     <div style={{maxWidth:'1200px'}} className="container-fluid mt-5 ">
-          <InfiniteCarousel
-          autoCycle={true} 
-        breakpoints={[
-          {
-            breakpoint: 500,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-            },
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-            },
-          },
-        ]}
-        dots={true}
-        showSides={true}
-        sidesOpacity={0.5}
-        sideSize={0.1}
-        slidesToScroll={4}
-        slidesToShow={4}
-        scrollOnDevice={false}
-      >
-        {props.posters.map((poster, index) => {
-          return (
-            <div key={index}>
-              <img
-                alt={poster.alt}
-                src={poster.img}
-              />
-            </div>
-          )
-        })}
-      </InfiniteCarousel>
+    <InfiniteCarousel
+    breakpoints={[
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+    ]}
+    dots={true}
+    showSides={true}
+    sidesOpacity={.5}
+    sideSize={.1}
+    slidesToScroll={4}
+    slidesToShow={4}
+    scrollOnDevice={true}
+  >
+    <div>
+      <img
+        alt=''
+        src="https://haintheme.com/demo/html/bustter/images/uploads/mv-item1.jpg"
+      />
+    </div>
+    <div>
+      <img
+        alt=''
+        src="https://haintheme.com/demo/html/bustter/images/uploads/mv-item1.jpg"
+      />
+    </div>
+    <div>
+      <img
+        alt=''
+        src="https://haintheme.com/demo/html/bustter/images/uploads/mv-item1.jpg"
+      />
+    </div>
+    <div>
+      <img
+        alt=''
+        src="https://haintheme.com/demo/html/bustter/images/uploads/mv-item1.jpg"
+      />
+    </div>
+   
+  </InfiniteCarousel>
     </div>
   )
 }
