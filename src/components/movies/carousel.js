3691cec227e5d8b748c6c4af20ab42befccf5df2
javascript01fan <ejrojas@icloud.com>
2019-11-14 19:45:00 -0500

    carousel bug fixed

--- src/components/movies/carousel.js
+++ src/components/movies/carousel.js
@@ -1,59 +1,31 @@
-import React from 'react'
-import InfiniteCarousel from "react-leaf-carousel"
-export default function Carousel(props) {
+import React from "react"
+import Carousel from "react-bootstrap/Carousel"
+
+export default function carousel(props) {
   return (
-    <div style={{maxWidth:'1200px'}} className="container-fluid mt-5 ">
-    <InfiniteCarousel
-    breakpoints={[
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-    ]}
-    dots={true}
-    showSides={true}
-    sidesOpacity={.5}
-    sideSize={.1}
-    slidesToScroll={4}
-    slidesToShow={4}
-    scrollOnDevice={true}
-  >
-    <div>
-      <img
-        alt=''
-        src="https://haintheme.com/demo/html/bustter/images/uploads/mv-item1.jpg"
-      />
-    </div>
-    <div>
-      <img
-        alt=''
-        src="https://haintheme.com/demo/html/bustter/images/uploads/mv-item1.jpg"
-      />
-    </div>
-    <div>
-      <img
-        alt=''
-        src="https://haintheme.com/demo/html/bustter/images/uploads/mv-item1.jpg"
-      />
-    </div>
-    <div>
-      <img
-        alt=''
-        src="https://haintheme.com/demo/html/bustter/images/uploads/mv-item1.jpg"
-      />
-    </div>
-   
-  </InfiniteCarousel>
+    <div
+      style={{
+        maxWidth: "1200px",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+      }}
+      className="container-fluid mt-5 "
+    >
+      <Carousel style={{ width: "400px" }}>
+        {props.posters.map((element, index) => {
+          return (
+            <Carousel.Item key={index}>
+              <img
+                style={{ width: "400px" }}
+                className="d-block "
+                src={element.img}
+                alt=""
+              />
+            </Carousel.Item>
+          )
+        })}
+      </Carousel>
     </div>
   )
 }
